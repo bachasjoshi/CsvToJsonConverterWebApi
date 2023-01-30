9c8c46897d9b25c779a36984ab492cdf684e39ec
Bachas Joshi <bachasjoshi11@gmail.com>
2023-01-30 11:17:54 +0545

    Add project files.

---
+++ CsvToJsonConverterWebApi/Controllers/CsvToJson.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CsvToJsonConverterWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CsvToJson : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Convert(IFormFile file)
+        {
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                var csvReader = new CsvReader(reader, System.Globalization.CultureInfo.CurrentCulture);
+                var records = csvReader.GetRecords<dynamic>();
+                var json = JsonConvert.SerializeObject(records);
+                return Ok(json);
+            }
+        }
+       
+        }
+    }
+
